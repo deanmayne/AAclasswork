6b6d9308257a16536b7e14058a5c4e0ce7aec8d8
Dean Mayne <deancmayne@gmail.com>
2020-10-16 17:04:18 -0700

    W4D5

--- W4D5/classes.rb
+++ W4D5/classes.rb
@@ -61,6 +61,107 @@ end
 
 class StackQueue
     def initialize
-        
+        @stack1 = MyStack.new
+        @stack2 = MyStack.new
     end
+
+    def size
+        @stack1.size + @stack2.size
+    end
+
+    def empty?
+        @stack1.empty? && @stack2.empty?
+    end
+
+    def enqueue(value)
+        @stack1.push(value)
+    end
+
+    def dequeue
+        if !@stack2.empty? 
+            @stack2.pop
+        else
+            @stack2 << @stack1.pop until @stack1.empty
+            @stack2.pop
+        end
+
+    end
+end
+
+class MinMaxStack
+    def initialize
+        @store = []
+    end
+
+    def size
+        @store.length
+    end
+
+    def empty?
+        @store.length == 0
+    end
+
+    def peek
+        @store.last
+    end
+
+    def push(value)
+        if self.empty?
+            @store << {"value" => value, "min" => value, "max" => value}
+        else
+            min = self.min
+            max = self.max
+            if min > value
+                min = value
+            end
+            if max < value
+                max = value
+            end
+            @store << {"value" => value, "min" => min, "max" => max}
+        end
+    end
+    
+    def min 
+        self.peek["min"]
+    end
+
+    def max 
+        self.peek["max"]
+    end
+
+    def pop
+        @store.pop
+    end
+
+end
+
+class MinMaxStackQueue
+
+    def initialize
+        @stack1 = MinMaxStack.new
+        @stack2 = MinMaxStack.new
+    end
+
+    def size
+        @stack1.size + @stack2.size
+    end
+
+    def empty?
+        @stack1.empty? && @stack2.empty?
+    end
+
+    def enqueue(value)
+        @stack1.push(value)
+    end
+
+    def dequeue
+        if !@stack2.empty? 
+            @stack2.pop
+        else
+            @stack2 << @stack1.pop until @stack1.empty
+            @stack2.pop
+        end
+
+    end
+
 end
--- W4D5/max_windowed_range.rb
+++ W4D5/max_windowed_range.rb
@@ -12,8 +12,21 @@ def windowed_max_range(arr, w)
     range
 end
 
-p windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
-p windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
-p windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
-p windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
+# p windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
 
+
+def windowed_max_range_v2(arr,w)
+    stack = MinMaxStackQueue.new
+    
+
+end
+
+
+
+p windowed_max_range_v2([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
+p windowed_max_range_v2([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
+p windowed_max_range_v2([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
+p windowed_max_range_v2([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
